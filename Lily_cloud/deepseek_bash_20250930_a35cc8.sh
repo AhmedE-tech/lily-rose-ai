47e089af48cf6da78c8e1cd695b91a0864033e39
AhmedE-tech <ahmedessam@outlook.de>
2025-09-30 18:09:54 +0300

    Delete Lily_cloud/deepseek_bash_20250930_a35cc8.sh

--- Lily_cloud/deepseek_bash_20250930_a35cc8.sh
+++
@@ -1,7 +0,0 @@
-cd Lily-Cloud
-git init
-git add README.md
-git commit -m "Initial commit: Lily Rose AI Assistant"
-git branch -M main
-git remote add origin https://github.com/AhmedE-tech/lily-rose-ai.git
-git push -u origin main
